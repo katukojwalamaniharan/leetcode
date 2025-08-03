c6549532db06647d2666ad96604bdd3b9baf2bc5
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-08-03 16:22:42 +0530

    Time: 2 ms (92.62%) | Memory: 47.7 MB (10.87%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,36 +1,26 @@
 class Solution {
-    public int bin(int[] numbers,int a,int b,int req){
-            while(a<=b){
-                int mid=a+(b-a)/2;
-            if(numbers[mid]==req){
-                return mid;
-            }else if(numbers[mid]<req){
-                a=mid+1;
-            }else{
-                b=mid-1;
-            }
-        }
-        return -1;
-    }
     public int[] twoSum(int[] numbers, int target) {
-        int n=numbers.length;
-        int a=0;
-        int b=n-1;
-        for(int i=0;i<n;i++){
-            if(numbers[i]<=target){
-                int req=target-numbers[i];
-                int inx=bin(numbers,i+1,n-1,req);
-                if(inx!=-1){
-                    return new int[] {i+1,inx+1};
-                }
-            }else if(target<0){
-                int req=target-numbers[i];
-                int inx=bin(numbers,i+1,n-1,req);
-                if(inx!=-1){
-                    return new int[] {i+1,inx+1};
-                }
+        int l=0;
+        int[] arr={-1,-1};
+        int temp=0;
+        int r=numbers.length-1;
+        while(l<r){
+            temp=numbers[l]+numbers[r];
+            
+             if(temp==target){
+                arr[0]=l+1;
+                arr[1]=r+1;
+                return arr;
             }
+            else if(temp>target){
+                r--;
+            }
+            else{
+                l++;
+            }
+
         }
-        return new int[] {-1,-1};
+
+        return arr;
     }
 }
