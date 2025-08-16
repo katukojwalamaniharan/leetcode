8dad01d3fbdb28754ee58751d1d8de56516e2162
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-08-16 18:52:46 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (86.42%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,14 +1,20 @@
 class Solution {
-        public int maximum69Number (int num) {
-            String s=num+"";
-            char[] arr=s.toCharArray();
-            for(int i=0;i<s.length();i++){
-                if(arr[i]=='6'){
-                    arr[i]='9';
-                    break;
-                }
+    public int maximum69Number(int num) {
+        int temp = num;
+        int place = 1;           // digit place (1, 10, 100...)
+        int firstSixPlace = 0;   // position of first 6 from right
+
+        // Find the leftmost 6 by traversing digits from right to left
+        while (temp > 0) {
+            int digit = temp % 10;
+            if (digit == 6) {
+                firstSixPlace = place; // store the place of the first 6
             }
-            String ans=new String(arr);
-            return Integer.parseInt(ans);
+            temp /= 10;
+            place *= 10;
+        }
+
+        // If there was a 6, change it to 9
+        return num + firstSixPlace * 3; // 9 - 6 = 3
     }
 }
