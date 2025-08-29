71c5c31852d9ffb8bd425801aa2ed8a3b99be3a1
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-08-29 21:46:03 +0530

    Time: 3 ms (75.93%), Space: 50.5 MB (27.02%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(hm.containsKey(nums[i])){
-                hm.put(nums[i],hm.get(nums[i])+1);
+        int cd1=-1;
+        int cd2=-1;
+        int count1=0;
+        int count2=0;
+        int i=0;
+        while(i<nums.length){
+            int num=nums[i];
+            if(num==cd1){
+                count1++;
+            }else if(num==cd2){
+                count2++;
+            }else if(count1==0){
+                cd1=num;
+                count1++;
+            }else if(count2==0){
+                cd2=num;
+                count2++;
             }else{
-                hm.put(nums[i],1);
+                count1--;
+                count2--;
             }
+            i++;
         }
-        List<Integer> ans=new ArrayList<>();
-        for (Map.Entry<Integer,Integer> entry : hm.entrySet()) {
-            if (entry.getValue() > nums.length / 3) {
-                ans.add(entry.getKey());
-            }
+        ArrayList<Integer> ans=new ArrayList<>();
+        int c1=0;
+        int c2=0;
+        for(i=0;i<nums.length;i++){
+            if(nums[i]==cd1) c1++;
+            if(nums[i]==cd2) c2++;
+        }
+        if(cd1==cd2){
+            ans.add(cd1);
+        }else{
+            if(c1>nums.length/3) ans.add(cd1);
+            if(c2>nums.length/3) ans.add(cd2);
         }
         return ans;
     }
