15e98c33132e957c013676f18b2d8e44d16bf9b5
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-09-06 18:58:19 +0530

    Time: 26 ms (37.63%), Space: 46.8 MB (25.41%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,15 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n=nums.length;
-        int count=0;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                }
-            }
+        Map<Long, Integer> freq = new HashMap<>(); // prefixSum -> count of occurrences
+        freq.put(0L, 1);           // base: one way to have prefix sum 0 (empty prefix)
+        long sum = 0L;
+        int count = 0;
+
+        for (int num : nums) {
+            sum += num;                       // current prefix sum
+            long need = sum - (long) k;       // we want previous prefixSum == need
+            count += freq.getOrDefault(need, 0);
+            freq.put(sum, freq.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
