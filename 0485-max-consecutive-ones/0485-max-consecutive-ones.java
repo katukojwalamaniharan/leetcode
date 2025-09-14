ddbed72343eed3f19b5393ccdeac6c29bf244543
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-09-14 14:43:58 +0530

    Time: 3 ms (58.25%), Space: 50.3 MB (47.45%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -3,14 +3,25 @@ class Solution {
         int l=0;
         int ans=0;
         int sum=0;
-        for(int r=0;r<nums.length;r++){
-            sum+=nums[r];
-            while(r-l+1!=sum){
-                sum-=nums[l];
-                l++;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                if(sum>ans){
+                    ans=sum;
+                }
+                sum=0;
+            }else{
+                sum++;
             }
             ans=Math.max(ans,sum);
         }
+        // for(int r=0;r<nums.length;r++){
+        //     sum+=nums[r];
+        //     while(r-l+1!=sum){
+        //         sum-=nums[l];
+        //         l++;
+        //     }
+        //     ans=Math.max(ans,sum);
+        // }
         return ans;
     }
 }
