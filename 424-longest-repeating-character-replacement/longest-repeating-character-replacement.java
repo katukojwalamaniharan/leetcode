824632fb6c15b413d92722c2909c648b160392d9
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-09-15 19:04:23 +0530

    Time: 6 ms (98.44%) | Memory: 45.2 MB (32.73%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -4,14 +4,18 @@ class Solution {
         int n=s.length();
         int mfq=0;
         int ans=0;
-        HashMap<Character,Integer> hm=new HashMap<>();
+        // HashMap<Character,Integer> hm=new HashMap<>();
+        int[] a=new int[26];
         while(r<n){
             char c=s.charAt(r);
-            hm.put(c,hm.getOrDefault(c,0)+1);
-            mfq=Math.max(mfq,hm.get(c));
+            // hm.put(c,hm.getOrDefault(c,0)+1);
+            a[c-'A']++;
+            // mfq=Math.max(mfq,hm.get(c));
+            mfq=Math.max(mfq,a[c-'A']);
             while((r-l+1)-mfq>k){
-                hm.put(s.charAt(l),hm.get(s.charAt(l))-1);
-                if(hm.get(s.charAt(l))==0) hm.remove(s.charAt(l));
+                // hm.put(s.charAt(l),hm.get(s.charAt(l))-1);
+                // if(hm.get(s.charAt(l))==0) hm.remove(s.charAt(l));
+                a[s.charAt(l)-'A']--;
                 l++;
             }
             ans=Math.max(ans,r-l+1);
