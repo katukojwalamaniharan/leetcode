6593d81a8d79efe4da43785971e97b5aa76039ba
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-09-15 10:46:19 +0530

    Time: 50 ms (52.47%), Space: 55.4 MB (32.05%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,3 +1,8 @@
+/*
+
+
+
+
 class Solution {
     public int totalFruit(int[] fruits) {
         int n=fruits.length;
@@ -13,7 +18,68 @@ class Solution {
                 }
                 l++;
             }
-            // ans=Math.max(ans,r-l+1);
+            if((r-l+1)>ans) ans=r-l+1;
+        }
+        return ans;
+    }
+}
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n=fruits.length;
+        int l=0,r=0;
+        int ans=0;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(r=0;r<n;r++){
+            hm.put(fruits[r],hm.getOrDefault(fruits[r],0)+1);
+            while(hm.size()>2){
+                hm.put(fruits[l],hm.get(fruits[l])-1);
+                if(hm.get(fruits[l])==0){
+                    hm.remove(fruits[l]);
+                }
+                l++;
+            }
+            if((r-l+1)>ans) ans=r-l+1;
+        }
+        return ans;
+    }
+}
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n=fruits.length;
+        int l=0,r=0;
+        int ans=0;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(r=0;r<n;r++){
+            hm.put(fruits[r],hm.getOrDefault(fruits[r],0)+1);
+            while(hm.size()>2){
+                hm.put(fruits[l],hm.get(fruits[l])-1);
+                if(hm.get(fruits[l])==0){
+                    hm.remove(fruits[l]);
+                }
+                l++;
+            }
+            if((r-l+1)>ans) ans=r-l+1;
+        }
+        return ans;
+    }
+}
+
+ */
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n=fruits.length;
+        int l=0,r=0;
+        int ans=0;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(r=0;r<n;r++){
+            hm.put(fruits[r],hm.getOrDefault(fruits[r],0)+1);
+            while(hm.size()>2){
+                hm.put(fruits[l],hm.get(fruits[l])-1);
+                if(hm.get(fruits[l])==0){
+                    hm.remove(fruits[l]);
+                }
+                l++;
+            }
             if((r-l+1)>ans) ans=r-l+1;
         }
         return ans;
