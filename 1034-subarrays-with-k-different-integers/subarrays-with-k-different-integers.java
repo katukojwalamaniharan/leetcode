e34c5b09855ee40eae3ff6782e285805d0fc69f6
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-09-19 19:08:36 +0530

    Time: 5 ms (99.08%) | Memory: 46.9 MB (87.32%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -7,16 +7,22 @@ class Solution {
         int n=nums.length;
         int count=0;
         if(k==0) return 0;
-        HashMap<Integer,Integer> hm=new HashMap<>();
+        // HashMap<Integer,Integer> hm=new HashMap<>();
+        int[] freq= new int[nums.length+1];
+        int d=0;
         while(r<n){
             int num=nums[r];
-            hm.put(num,hm.getOrDefault(num,0)+1);
-            while(hm.size()>k){
-                hm.put(nums[l],hm.get(nums[l])-1);
-                if(hm.get(nums[l])==0) hm.remove(nums[l]);
+            // hm.put(num,hm.getOrDefault(num,0)+1);
+            if(freq[num]==0) d++; // to track the distinct elements.
+            freq[num]++;
+            while(d>k){
+                // hm.put(nums[l],hm.get(nums[l])-1);
+                // if(hm.get(nums[l])==0) hm.remove(nums[l]);
+                freq[nums[l]]--;
+                if(freq[nums[l]]==0) d--;
                 l++;
             }
-            if(hm.size()<=k) count+=r-l+1;
+            if(d<=k) count+=r-l+1;
             r++;
         }
         return count;
