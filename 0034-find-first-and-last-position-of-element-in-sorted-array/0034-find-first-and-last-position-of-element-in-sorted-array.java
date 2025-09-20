ace27cba5a456bcb84e25d30d748885474ed4e26
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-09-20 21:50:42 +0530

    Time: 0 ms (100%), Space: 46.1 MB (31.07%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int left = lb(nums, target);
-        
-        // If target is not present
-        if(left == nums.length || nums[left] != target) 
-            return new int[]{-1, -1};
-        
-        int right = ub(nums, target) - 1;
-        return new int[]{left, right};
+        if(nums.length==0) return new int[]{-1,-1};
+        int left=lb(nums , target);
+        int right=ub(nums , target);
+        if(left==nums.length||nums[left]!=target) return new int[]{-1,-1};
+        return new int[]{left,right-1};
     }
-    
-    // Lower bound → first index where nums[i] >= target
-    public int lb(int[] nums, int target){
-        int l = 0, h = nums.length - 1;
-        int ans = nums.length; // if not found, points past last element
-        while(l <= h){
-            int mid = l + (h - l) / 2;
-            if(nums[mid] >= target){
-                ans = mid;
-                h = mid - 1;
-            } else {
-                l = mid + 1;
+    public int lb(int[] nums,int target){
+        int l=0;
+        int h=nums.length-1;
+        int ans=nums.length;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            // ans=nums.length;
+            if(nums[mid]>=target){
+                ans=mid;
+                h=mid-1;
+            }else{
+                l=mid+1;
             }
         }
         return ans;
     }
-    
-    // Upper bound → first index where nums[i] > target
-    public int ub(int[] nums, int target){
-        int l = 0, h = nums.length - 1;
-        int ans = nums.length; // if not found, points past last element
-        while(l <= h){
-            int mid = l + (h - l) / 2;
-            if(nums[mid] > target){
-                ans = mid;
-                h = mid - 1;
-            } else {
-                l = mid + 1;
+    public int ub(int[] nums,int target){
+        int l=0;
+        int h=nums.length-1;
+        int anss=nums.length;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            // int anss=nums.length;
+            if(nums[mid]>target){
+                anss=mid;
+                h=mid-1;
+            }else{
+                l=mid+1;
             }
         }
-        return ans;
+        return anss;
     }
 }
