c5a6e7283ce12c67438118db63742712813efc8c
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-01-03 22:46:42 +0530

    Time: 82 ms (32.73%), Space: 84.2 MB (15.3%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,25 +1,36 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        if(heights.length==1) return heights[0];
-        return 2*secondLargest(heights);
-    }
-static int secondLargest(int[] arr) {
-    if (arr == null || arr.length < 2) {
-        return -1; // not possible
-    }
-
-    int largest = Integer.MIN_VALUE;
-    int secondLargest = Integer.MIN_VALUE;
-
-    for (int x : arr) {
-        if (x > largest) {
-            secondLargest = largest;
-            largest = x;
-        } else if (x < largest && x > secondLargest) {
-            secondLargest = x;
+        int ans = Integer.MIN_VALUE;
+        int n = heights.length;
+        int[] ns = nsi(heights);
+        int[] ps = psi(heights);
+        for(int i=0;i<n;i++){
+            int temp = heights[i]*(ns[i]-ps[i]-1);
+            ans=Math.max(ans,temp);
         }
+        return ans;
     }
-
-    return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
-}
+    public int[] nsi(int[] h){
+        int n=h.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans,n);
+        Stack<Integer> wr = new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!wr.empty() && h[i] < h[wr.peek()]) ans[wr.pop()]=i;
+            wr.add(i);
+        }
+        return ans;
+    }
+    public int[] psi(int[] h){
+       int  n=h.length;
+        int[] ans = new int[n];//created ans array
+        Arrays.fill(ans,-1); //filler with 
+        Stack<Integer> wr = new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            //checking does the stack is not empty and current element is 
+            while(!wr.empty() && h[i] < h[wr.peek()]) ans[wr.pop()]=i;
+            wr.add(i);
+        }
+        return ans;
+    }
 }
