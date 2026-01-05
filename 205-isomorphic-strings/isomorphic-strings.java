bf7bde58629bdd5e45d85ba50956dd6d1cac637d
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-01-05 21:22:08 +0530

    Time: 12 ms (54.10%) | Memory: 44.1 MB (26.27%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,12 +1,4 @@
 class Solution {
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
     public boolean isIsomorphic(String s, String t) {
         HashMap<Character,Character> hm = new HashMap<>();
         for(int i=0;i<s.length();i++){
