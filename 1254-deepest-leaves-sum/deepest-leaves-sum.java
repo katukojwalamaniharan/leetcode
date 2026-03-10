b3ebf6f36747cf2a92e3f4ac6b8e90b4aab2482b
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-03-10 16:33:23 +0530

    Time: 7 ms (66.51%) | Memory: 48.7 MB (7.08%) - LeetSync

--- 1254-deepest-leaves-sum/deepest-leaves-sum.java
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.java
@@ -1,27 +1,29 @@
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
-        int d = depth(root);
+        // int d = depth(root);
         int sum = 0;
-        int depth = 0;
+        // int depth = 0;
         Queue<TreeNode> q = new LinkedList<>();
         if(root == null) return 0;
         q.add(root);
         while(!q.isEmpty()){
             int size = q.size();
-            depth++;
+            // depth++;
+            sum = 0;
             for(int i=1;i<=size;i++){
                 TreeNode temp = q.poll();
-                if(depth == d) sum+=temp.val;
+                // if(depth == d) 
+                sum+=temp.val;
                 if(temp.right!=null) q.add(temp.right);
                 if(temp.left!=null) q.add(temp.left);
             }
         }
         return sum;
     }
-    int depth(TreeNode root){
-        if(root == null) return 0;
-        int r = depth(root.right);
-        int l = depth(root.left);
-        return 1+Math.max(l,r);
-    }
+    // int depth(TreeNode root){
+    //     if(root == null) return 0;
+    //     int r = depth(root.right);
+    //     int l = depth(root.left);
+    //     return 1+Math.max(l,r);
+    // }
 }
