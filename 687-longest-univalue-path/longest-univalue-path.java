85ea8ef1b86ec63db4567020e9d1595c64fff5c3
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-03-12 13:00:06 +0530

    Time: 2 ms (98.59%) | Memory: 50.2 MB (40.67%) - LeetSync

---
+++ 687-longest-univalue-path/longest-univalue-path.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    public int longestUnivaluePath(TreeNode root) {
+        help(root);
+        return ans;
+    }
+    int help (TreeNode curr){
+        if(curr == null) return 0;
+        int l = help(curr.left);
+        int r = help(curr.right);
+        if(curr.left!=null && curr.val == curr.left.val)
+            l++;
+        else
+            l=0;
+        if(curr.right!=null && curr.val == curr.right.val)
+            r++;
+        else
+            r=0;
+        ans = Math.max(ans,l+r);
+        return Math.max(l,r);
+    }
+}
