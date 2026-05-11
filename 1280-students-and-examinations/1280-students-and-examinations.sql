d848f386abf15b00d7a94e33d1232af57efb4624
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-05-11 18:30:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,7 +1,7 @@
-select s.student_id , s.student_name , ss.subject_name ,count(e.subject_name) as attended_exams
+select s.student_id , s.student_name , ss.subject_name , count(e.subject_name) as attended_exams
 from students s
 cross join subjects ss
 left join examinations e
 on s.student_id = e.student_id and ss.subject_name = e.subject_name
-group by s.student_id , s.student_name , ss.subject_name
-order by s.student_id , s.student_name , ss.subject_name
+group by s.student_id,s.student_name,ss.subject_name
+order by s.student_id,s.student_name,ss.subject_name
