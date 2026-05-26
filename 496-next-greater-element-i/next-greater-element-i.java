c7be45caeed5640017f5c75a2526437028792912
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-05-26 22:56:33 +0530

    Time: 4 ms (75.80%) | Memory: 45.4 MB (35.43%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -13,13 +13,10 @@ class Solution {
 
             if(st.isEmpty()){
                 hm.put(num,-1);
-                st.push(num);
             }else if(st.peek()>num){
                 hm.put(num,st.peek());
-                st.push(num);
-            }else{
-                st.push(num);
             }
+            st.push(num);
         }
         for(int i=0;i<nums1.length;i++){
             ans[i] = hm.get(nums1[i]);
