f1fc014d1407e2b4ef5435c8c91f288357c08319
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-05-27 11:24:53 +0530

    Time: 1 ms (58.73%) | Memory: 48.9 MB (9.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,28 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        int ans=0;
-        //int water_level=0;
-        int[] prefix=new int[height.length];
-        int[] suffix=new int[height.length];
-        prefix[0]=height[0];
-        for(int i=1;i<height.length;i++){
-            prefix[i]=Math.max(prefix[i-1],height[i]);
+        int total = 0;
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+        int leftMax  = 0;
+        int rightMax = 0;
+        for(int i=0;i<height.length;i++){
+            leftMax = Math.max(leftMax,height[i]);
+            left[i] = leftMax;
         }
-        suffix[height.length-1]=height[height.length-1];
-        for(int i=height.length-2;i>=0;i--){
-            suffix[i]=Math.max(suffix[i+1],height[i]);
+        for(int i=height.length-1;i>=0;i--){
+            rightMax = Math.max(rightMax,height[i]);
+            right[i] = rightMax;
         }
         for(int i=0;i<height.length;i++){
-                int left=prefix[i];
-                int right=suffix[i];
-            if((left>height[i])&&(right>height[i])){
-                int water_level=Math.min(right,left);
-                int water=water_level-height[i];
-                if(water>0){
-                    ans+=water;
-                }
+            if(height[i]<left[i] && height[i]<right[i]){
+                total+=Math.min(left[i],right[i])-height[i];
             }
         }
-        return ans;
+        return total;
     }
 }
