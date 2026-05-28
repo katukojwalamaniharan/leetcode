e2958ac4fb4b66e78eced9acd370c5b40745eedb
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-05-29 02:18:41 +0530

    Time: 100 ms (11.76%) | Memory: 80.5 MB (89.36%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,33 +1,13 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums,0,nums.length-1);
-        return nums;
-    }
-    public void mergeSort(int[] nums,int low,int high){
-        if(low>=high) return;
-        int mid = low+(high-low)/2;
-        mergeSort(nums,low,mid);
-        mergeSort(nums,mid+1,high);
-        merge(nums,low,mid,high);
-    }
-    public void merge(int[] nums,int low,int mid,int high){
-        int n1 = mid-low+1;
-        int n2 = high - mid;
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-        System.arraycopy(nums,low,left,0,n1);
-        System.arraycopy(nums,mid+1,right,0,n2);
-        int i=0;
-        int j=0;
-        int k = low;
-        while(i<n1&&j<n2){
-            if (left[i] <= right[j]) {
-                nums[k++] = left[i++];
-            } else {
-                nums[k++] = right[j++];
-            }
+        int[] ans = new int[nums.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num:nums){
+            pq.offer(num);
         }
-        while (i < n1) nums[k++] = left[i++];
-        while (j < n2) nums[k++] = right[j++];
+        for(int i=0;i<ans.length;i++){
+            ans[i] = pq.poll();
+        }
+        return ans;
     }
 }
