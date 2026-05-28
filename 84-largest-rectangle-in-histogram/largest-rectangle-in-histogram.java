fc763fcee034f5f50c27579e83fd0260e5a63d0c
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-05-28 16:19:16 +0530

    Time: 112 ms (10.63%) | Memory: 75.9 MB (96.14%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,34 +1,42 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int ans = Integer.MIN_VALUE;
-        int n = heights.length;
-        int[] ps = psi(heights);
-        int[] ns = nsi(heights);
-        for(int i=0;i<n;i++){
-            int temp = heights[i]*(ps[i]-ns[i]-1);
-            ans=Math.max(ans,temp);
+        int[] pse = pse(heights);
+        int[] nse = nse(heights);
+        int ans = 0;
+        for(int i=0;i<heights.length;i++){
+            ans = Math.max(ans,(nse[i]-pse[i]-1)*heights[i]);
         }
         return ans;
     }
-    public int[] psi(int[] h){
-        int n=h.length;
-        int[] ans = new int[n];
-        Arrays.fill(ans,n);
-        Stack<Integer> wr = new Stack<>();
-        for(int i=0;i<n;i++){
-            while(!wr.empty() && h[i] < h[wr.peek()]) ans[wr.pop()]=i;
-            wr.add(i);
+    public static int[] pse(int[] arr){
+        int[] ans = new int[arr.length];
+        Arrays.fill(ans,-1);
+        Stack<Integer> s = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            int num = arr[i];
+            while(!s.isEmpty() && arr[s.peek()]>=num) s.pop();
+            if(s.isEmpty()){
+
+            }else{
+                ans[i]=s.peek();
+            }
+            s.push(i);
         }
         return ans;
     }
-    public int[] nsi(int[] h){
-       int  n=h.length;
-        int[] ans = new int[n];//created ans array
-        Arrays.fill(ans,-1); //filler with 
-        Stack<Integer> wr = new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!wr.empty() && h[i] < h[wr.peek()]) ans[wr.pop()]=i;
-            wr.add(i);
+    public static int[] nse(int[] arr){
+        int[] ans = new int[arr.length];
+        Arrays.fill(ans,arr.length);
+        Stack<Integer> s = new Stack<>();
+        for(int i=arr.length-1;i>=0;i--){
+            int num = arr[i];
+            while(!s.isEmpty() && arr[s.peek()]>=num) s.pop();
+            if(s.isEmpty()){
+
+            }else{
+                ans[i]=s.peek();
+            }
+            s.push(i);
         }
         return ans;
     }
