dca0136da35bcbb988c416878419254f5cac1ac6
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-08-17 15:01:57 +0530

    Time: 8 ms (91.72%) | Memory: 48.8 MB (86.02%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         if(intervals.length<2) return intervals;
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
         ArrayList<int[]> al = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         int[] prev = intervals[0];
         for(int i=1;i<intervals.length;i++){
             int[] curr = intervals[i];
             if(prev[1]>=curr[0]){
-                prev = new int[]{prev[0],Math.max(curr[1],prev[1])};
-                // al.add(prev);
+                prev = new int[]{prev[0],Math.max(prev[1],curr[1])};
             }else{
                 al.add(prev);
                 prev = curr;
@@ -16,7 +15,9 @@ class Solution {
         }
         al.add(prev);
         int[][] ans = new int[al.size()][2];
-        for(int i=0;i<al.size();i++) ans[i] = al.get(i);
+        for(int i=0;i<ans.length;i++){
+            ans[i]=al.get(i);
+        }
         return ans;
     }
 }
