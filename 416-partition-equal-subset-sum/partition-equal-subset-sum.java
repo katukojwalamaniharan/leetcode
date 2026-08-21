538f52c6470e015438a14a45248faac9ad0d5b96
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-08-21 21:10:22 +0530

    Time: 57 ms (41.00%) | Memory: 47.1 MB (64.97%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
         int sum = 0;
         for(int i:nums) sum+=i;
         if(sum%2 != 0) return false;
         int target = sum/2;
-        return checkSubsequenceSum(nums,target);
-    }
-    public boolean checkSubsequenceSum(int[] arr, int k) {
-        // code here
-        Boolean[][] dp = new Boolean[arr.length][k + 1];
-        return solve(0,arr,k,dp);
-        
-    }
-    public boolean solve(int i,int[] arr, int k,Boolean[][] dp){
-        if(k == 0) return true;
-        if(i == arr.length || k<0) return false;
-        if (dp[i][k] != null) {
-            return dp[i][k];
+        boolean dp[][] = new boolean[n+1][target+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
         }
-        boolean pick = solve(i+1,arr,k-arr[i],dp);
-        boolean notPick = solve(i+1,arr,k,dp);
-        return dp[i][k]=pick||notPick;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                boolean inc = false,exc = false;
+                if(nums[i-1]<=j){
+                    inc = true&&dp[i-1][j-nums[i-1]];
+                }
+                exc = dp[i-1][j];
+                dp[i][j] = exc||inc;
+            }
+        }
+        return dp[n][target];
     }
 }
