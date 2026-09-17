1353501fa0165339236d6ecc06254990c5a5a015
maniharanchary <148434634+katukojwalamaniharan@users.noreply.github.com>
2026-09-17 21:38:59 +0530

    Time: 3 ms (56.15%) | Memory: 44.6 MB (46.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,19 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int i=0;
-        int j=s.length()-1;
+        s=s.toLowerCase();
+        int i = 0;
+        int j = s.length()-1;
         while(i<j){
-            if(!Character.isLetterOrDigit(s.charAt(i))){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
                 i++;
-                continue;
             }
-            if(!Character.isLetterOrDigit(s.charAt(j))){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
                 j--;
-                continue;
             }
-            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
-                return false;
-            }
+            if(s.charAt(i)!=s.charAt(j)) return false;
             i++;
             j--;
         }
